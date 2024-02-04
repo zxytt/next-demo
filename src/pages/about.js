63a64d0c581a6e2469ead728d975e1fdabcc0305
张晓渊 <zhangxiaoyuan@itcast.cn>
2024-02-04 16:31:31 +0800

    feat: '接口修改'

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,14 +4,14 @@ export default function About({ data }) {
   return (
     <ul>
       {
-        data.map(item => <li key={item.id}>{item.name}</li>)
+        data.map(item => <li key={item.id}>{item.title}</li>)
       }
     </ul>
   )
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://directus-t.itcast.cn/items/cars')
+  const res = await fetch('http://0.0.0.0:8055/items/news')
   const { data } = await res.json()
   return {
     props: {

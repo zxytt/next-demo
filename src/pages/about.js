a39ce7402ca9e9fefe96d979acff63238e6d5131
张晓渊 <zhangxiaoyuan@itcast.cn>
2024-02-20 17:53:34 +0800

    feat: 'xxxxxxx'

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,8 @@ export default function About({ data }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch('http://39.101.67.62:8055/items/news')
+  // const res = await fetch('http://39.101.67.62:8055/items/news')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DIRECTUS_API_URL}/news`)
   const { data } = await res.json()
   return {
     props: {
